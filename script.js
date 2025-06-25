b2aed9490cf1a58bcadf81af2d74e2188827e668
Leonardo Silva e Cruz <101243664+HiperD@users.noreply.github.com>
2025-06-25 20:28:19 -0300

    1111

--- script.js
+++ script.js
@@ -3,3 +3,14 @@ alert("oieeeeeeeeeee")
 alert("ele é limitado ao clique no ok pelo usuario")
 
 alert("Não é soccilvio config seu tempo de permanencia na tela")
+
+
+let numero = 25
+
+let chute;
+
+let idade;
+
+let nome;
+
+console.log(numero);
